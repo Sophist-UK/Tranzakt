92f3756837129fd774d66600f9353e12518c409b
Sophist <3001893+Sophist-UK@users.noreply.github.com>
2022-07-02 15:37:32 +0100

    Update TranzaktSchema.php
    Make table names lower-case (because they always are) and add cascade deletes to all relationships.

--- src/database/schema/TranzaktSchema.php
+++ src/database/schema/TranzaktSchema.php
@@ -14,7 +14,6 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use Database\Seeders\TranzaktSampleAppSeeder
 
 return new class extends Migration
 {
@@ -68,7 +67,7 @@ return new class extends Migration
 	 */
 	private function create_system_metadata()
 	{
-		Schema::create('Tranzakt_Databases', function(Blueprint $table) {
+		Schema::create('tranzakt_databases', function(Blueprint $table) {
 			$this->common_columns(
 				$table,
 				'Tranzact supports multiple database connections - default Laravel connection is connection 1'
@@ -85,7 +84,7 @@ return new class extends Migration
 			$table->unique(['host', 'port', 'database', 'deleted_at'], 'tranzakt_databases_unique');
 		});
 
-		Schema::create('Tranzakt_Applications', function(Blueprint $table) {
+		Schema::create('tranzakt_applications', function(Blueprint $table) {
 			$this->common_columns(
 				$table,
 				'Tranzact supports development of multiple applications with independent tables'
@@ -99,21 +98,21 @@ return new class extends Migration
 			$table->unique(['table_prefix', 'deleted_at'], 'tranzakt_applications_unique_table_prefix');
 		});
 
-		Schema::create('Tranzakt_Tags', function(Blueprint $table) {
+		Schema::create('tranzakt_tags', function(Blueprint $table) {
 			$this->common_columns(
 				$table,
 				'Tag names and descriptions',
 				False
 			);
 
-			$table->foreignId('app_id')->constrained('Tranzakt_Applications');
+			$table->foreignId('app_id')->constrained('tranzakt_applications');
 			$table->string('tag', 64);
 			$table->text('notes');
 
 			$table->unique(['app_id', 'tag', 'deleted_at'], 'tranzakt_tags_unique_app_id_tag');
 		});
 
-		Schema::create('Tranzakt_Taggables', function(Blueprint $table) {
+		Schema::create('tranzakt_taggables', function(Blueprint $table) {
 			$this->common_columns(
 				$table,
 				'Link Tags with tagged items',
@@ -123,7 +122,8 @@ return new class extends Migration
 			$table->foreignId('tag_id');
 			$table->morphs('taggable');
 
-			$table->foreign('tag_id')->references('id')->on('Tranzakt_Tags');
+			$table->foreign('tag_id')->references('id')->on('tranzakt_tags')
+				->onDelete('cascade');
 
 			$table->unique(['tag_id', 'taggable_id', 'taggable_type', 'deleted_at'], 'tranzakt_taggables_unique_tag_id_taggable');
 		});
@@ -134,7 +134,7 @@ return new class extends Migration
 	 */
 	private function create_table_metadata()
 	{
-		Schema::create('Tranzakt_Table_Areas', function(Blueprint $table) {
+		Schema::create('tranzakt_table_areas', function(Blueprint $table) {
 			$this->common_columns(
 				$table,
 				'Tranzact allows you to group tables into areas for diagramming and documentation purposes'
@@ -145,12 +145,13 @@ return new class extends Migration
 			$table->string('description');
 			$table->text('notes');
 
-			$table->foreign('application_id')->references('id')->on('Tranzakt_Applications');
+			$table->foreign('application_id')->references('id')->on('tranzakt_applications')
+				->onDelete('cascade');
 
 			$table->unique(['application_id', 'area', 'deleted_at'], 'tranzakt_table_areas_unique_app_id_area');
 		});
 
-		Schema::create('Tranzakt_Tables', function(Blueprint $table) {
+		Schema::create('tranzakt_tables', function(Blueprint $table) {
 			$this->common_columns(
 				$table,
 				'This table holds the list of tables'
@@ -179,15 +180,18 @@ return new class extends Migration
 				$table->json('canvas')->nullable();
 			}
 
-			$table->foreign('application_id')->references('id')->on('Tranzakt_Applications');
-			$table->foreign('database_id')->references('id')->on('Tranzakt_Databases');
-			$table->foreign('area_id')->references('id')->on('Tranzakt_Table_Areas');
+			$table->foreign('application_id')->references('id')->on('tranzakt_applications')
+				->onDelete('cascade');
+			$table->foreign('database_id')->references('id')->on('tranzakt_databases')
+				->onDelete('cascade');
+			$table->foreign('area_id')->references('id')->on('tranzakt_table_areas')
+				->onDelete('cascade');
 
 			$table->unique(['application_id', 'table', 'deleted_at'], 'tranzakt_tables_unique_app_id_table');
 			$table->unique(['database_id', 'full_name', 'deleted_at'], 'tranzakt_tables_unique_db_id_fullname');
 		});
 
-		Schema::create('Tranzakt_Table_Columns', function(Blueprint $table) {
+		Schema::create('tranzakt_table_columns', function(Blueprint $table) {
 			$this->common_columns(
 				$table,
 				'This table holds details of every column in every table.'
@@ -209,12 +213,12 @@ return new class extends Migration
 			$table->json('options')->nullable();
 			$table->text('notes');
 
-			$table->foreign('table_id')->references('id')->on('Tranzakt_Tables');
+			$table->foreign('table_id')->references('id')->on('tranzakt_tables');
 
 			$table->unique(['table_id', 'column', 'deleted_at'], 'tranzakt_columns_unique_table_id_column');
 		});
 
-		Schema::create('Tranzakt_Table_Indexes', function(Blueprint $table) {
+		Schema::create('tranzakt_table_indexes', function(Blueprint $table) {
 			$this->common_columns(
 				$table,
 				'Details of all indexes on all tables.'
@@ -227,10 +231,11 @@ return new class extends Migration
 			$table->enum('sort_order', ['ASC', 'DESC'])->default('ASC');
 			$table->text('notes');
 
-			$table->foreign('table_id')->references('id')->on('Tranzakt_Tables');
+			$table->foreign('table_id')->references('id')->on('tranzakt_tables')
+				->onDelete('cascade');
 		});
 
-		Schema::create('Tranzakt_Table_Seeds', function(Blueprint $table) {
+		Schema::create('tranzakt_table_seeds', function(Blueprint $table) {
 			$this->common_columns(
 				$table,
 				'Tranzakt supports the Laravel concept of seeds i.e. rows created when the tables are created'
@@ -239,10 +244,11 @@ return new class extends Migration
 			$table->foreignId('table_id');
 			$table->json('row_values');
 
-			$table->foreign('table_id')->references('id')->on('Tranzakt_Tables');
+			$table->foreign('table_id')->references('id')->on('tranzakt_tables')
+				->onDelete('cascade');
 		});
 
-		Schema::create('Tranzakt_Table_Relationships', function(Blueprint $table) {
+		Schema::create('tranzakt_table_relationships', function(Blueprint $table) {
 			$this->common_columns(
 				$table,
 				'This table holds details of the relationships between tables and supports Laravel polymorphic relationships'
@@ -262,8 +268,10 @@ return new class extends Migration
 				$table->json('canvas')->nullable();
 			}
 
-			$table->foreign('primary_table_id')->references('id')->on('Tranzakt_Tables');
-			$table->foreign('foreign_table_id')->references('id')->on('Tranzakt_Tables');
+			$table->foreign('primary_table_id')->references('id')->on('tranzakt_tables')
+				->onDelete('cascade');
+			$table->foreign('foreign_table_id')->references('id')->on('tranzakt_tables')
+				->onDelete('cascade');
 		});
 
 		echo "Tranzakt schema tables defined." .PHP_EOL;
@@ -281,12 +289,12 @@ return new class extends Migration
 	 */
 	private function drop_table_metadata()
 	{
-		Schema::dropIfExists('Tranzakt_Table_Relationships');
-		Schema::dropIfExists('Tranzakt_Table_Seeds');
-		Schema::dropIfExists('Tranzakt_Table_Indexes');
-		Schema::dropIfExists('Tranzakt_Table_Columns');
-		Schema::dropIfExists('Tranzakt_Tables');
-		Schema::dropIfExists('Tranzakt_Table_Areas');
+		Schema::dropIfExists('tranzakt_table_relationships');
+		Schema::dropIfExists('tranzakt_table_seeds');
+		Schema::dropIfExists('tranzakt_table_indexes');
+		Schema::dropIfExists('tranzakt_table_columns');
+		Schema::dropIfExists('tranzakt_tables');
+		Schema::dropIfExists('tranzakt_table_areas');
 	}
 
 	/**
@@ -294,9 +302,9 @@ return new class extends Migration
 	 */
 	private function drop_system_metadata()
 	{
-		Schema::dropIfExists('Tranzakt_Taggables');
-		Schema::dropIfExists('Tranzakt_Tags');
-		Schema::dropIfExists('Tranzakt_Applications');
-		Schema::dropIfExists('Tranzakt_Databases');
+		Schema::dropIfExists('tranzakt_taggables');
+		Schema::dropIfExists('tranzakt_tags');
+		Schema::dropIfExists('tranzakt_applications');
+		Schema::dropIfExists('tranzakt_databases');
 	}
 };
